363609e15865d4706d69637aeadda2bd71233242
Zia Syed <zia.syed@ericsson.com>
2012-06-02 16:27:02 -0700

    with a bit of animation

--- public/maps.js
+++ public/maps.js
@@ -1,4 +1,6 @@
 var mecca =new google.maps.LatLng(21.4167, 39.8167);
+var marker;
+
 function initialize() {
 
   var myOptions = {
@@ -8,9 +10,21 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"),
       myOptions);
-	var marker= new google.maps.Marker({
+  marker= new google.maps.Marker({
 		position: mecca,
-		title: "Center of the world"
+		title: "Center of the world",
+		animation: google.maps.Animation.DROP
+		
 	});
 	marker.setMap(map);
+	google.maps.event.addListener(marker, 'click', toggleBounce);
+  
+}
+function toggleBounce()
+{
+ if (marker.getAnimation() !==null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
